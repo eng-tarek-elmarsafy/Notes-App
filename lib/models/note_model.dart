e351574f7cc38b52fbabb0a4cb03b047253e9af3
eng-tarek-elmarsafy <tarekelmarsafy50@gmail.com>
2025-01-17 00:31:52 +0200

    Create NoteModel and add hive packages

---
+++ lib/models/note_model.dart
@@ -0,0 +1,11 @@
+class NoteModel {
+  final String title;
+  final String subTitle;
+  final String data;
+  final int color;
+  const NoteModel(
+      {required this.title,
+      required this.subTitle,
+      required this.data,
+      required this.color});
+}
